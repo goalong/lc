6826b0132441c1c93d0bb74367cafee11a9f74cd
along <along@myw-vicdeiMac.local>
2017-12-05 14:11:36 +0800

    add solution

--- 155.py
+++ 155.py
@@ -4,6 +4,8 @@ class MinStack(object):
         """
         initialize your data structure here.
         """
+        self.stack = []
+        self.min_stack = []
         
 
     def push(self, x):
@@ -11,24 +13,38 @@ class MinStack(object):
         :type x: int
         :rtype: void
         """
+        self.stack.append(x)
+        if self.min_stack:
+            if x <= self.min_stack[-1]:
+                self.min_stack.append(x)
+        else:
+            self.min_stack.append(x)
         
 
     def pop(self):
         """
         :rtype: void
         """
+        last = self.stack.pop()
+        if self.min_stack and self.min_stack[-1] == last:
+            self.min_stack.pop()
+        return last
         
 
     def top(self):
         """
         :rtype: int
         """
+        if self.stack:
+            return self.stack[-1]
         
 
     def getMin(self):
         """
         :rtype: int
         """
+        if self.min_stack:
+            return self.min_stack[-1]
         
 
 
@@ -38,3 +54,13 @@ class MinStack(object):
 # obj.pop()
 # param_3 = obj.top()
 # param_4 = obj.getMin()
+
+
+# stack = MinStack()
+# stack.push(0)
+# stack.push(1)
+# stack.push(0)
+
+# print stack.getMin()
+# stack.pop()
+# print stack.getMin()
