d9d5a2bcb10c8e1988cf15d7c67dfc13f65aaed8
along <along@myw-vicdeiMac.local>
2018-03-19 16:43:29 +0800

    add new solutions

---
+++ v2/76.py
@@ -0,0 +1,16 @@
+
+
+
+
+
+# todo
+
+class Solution:
+    def minWindow(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: str
+        """
+        # 较复杂，回头再做
+        pass
---
+++ v2/77.py
@@ -0,0 +1,24 @@
+
+
+
+
+
+class Solution(object):
+    def combine(self, n, k):
+        """
+        :type n: int
+        :type k: int
+        :rtype: List[List[int]]
+        """
+        # 6 star, 起初用dfs做的，结果一直超时，
+        if k > n:
+            return []
+        if k == 1:
+            return [[i + 1] for i in range(n)]
+        if n > k:
+            result = [r + [n] for r in self.combine(n - 1, k - 1)] + self.combine(n - 1, k)  # 有n的 和 没n的
+        else:
+            result = [r + [n] for r in self.combine(n - 1, k - 1)]  # 之所以有这个else, 是处理n=k的
+        return result
+
+# print(Solution().combine(4, 2))
