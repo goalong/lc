3b49c7f1e490e6f4a73d7dd938eb4222c2304b03
along <along@myw-vicdeiMac.local>
2018-03-26 19:11:27 +0800

    add new

---
+++ v2/111.py
@@ -0,0 +1,32 @@
+
+
+
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def minDepth(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        # 6 star, 分类讨论的情形比较巧妙，一开始做错了
+        if not root:
+            return 0
+        return self._minDepth(root)
+
+    def _minDepth(self, node):
+        if not node.left and not node.right:
+            return 1
+        elif node.left and node.right:
+            return min(self._minDepth(node.left), self._minDepth(node.right)) + 1
+        elif node.left:
+            return self._minDepth(node.left) + 1
+        else:
+            return self._minDepth(node.right) + 1
