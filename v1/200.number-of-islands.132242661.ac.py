dfac0f3622bfbfc2c5b6573a9fc9e0c3689c5261
goalong <goforalong@gmail.com>
2018-06-23 17:52:28 +0800

    add new

--- v1/200.number-of-islands.132242661.ac.py
+++ v1/200.number-of-islands.132242661.ac.py
@@ -38,8 +38,10 @@ class Solution(object):
                     count += 1
         return count
     def dfs(self, grid, i, j):
+        # 对于不在矩阵内的或者是位置上不是1的直接跳过
         if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[0]) or grid[i][j] != '1':
             return
+        # 对于位置上是1的，将其标注为-1，并向上下左右四个方向递归调用，因此一个岛屿的整个部分会全部变成-1，并且被计数上算作一个
         grid[i][j] = '-1'
         self.dfs(grid, i-1, j)
         self.dfs(grid, i, j - 1)
