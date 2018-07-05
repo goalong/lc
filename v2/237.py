cc56d74c1fdb26e7181ba20b8326e112746deaf4
along <along@myw-vicdeiMac.local>
2018-07-05 16:46:22 +0800

    add 237

---
+++ v2/237.py
@@ -0,0 +1,19 @@
+
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution(object):
+    def deleteNode(self, node):
+        """
+        :type node: ListNode
+        :rtype: void Do not return anything, modify node in-place instead.
+        """
+        # 2 star
+        if not node:
+            raise TypeError
+        if not node.next:
+            node = None
+        node.val, node.next = node.next.val, node.next.next
