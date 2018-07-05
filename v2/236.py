afa3ebba8e29ae5d5f4020a28270d7cb2ec2b94c
along <along@myw-vicdeiMac.local>
2018-07-05 16:24:21 +0800

    add 236

---
+++ v2/236.py
@@ -0,0 +1,27 @@
+
+
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def lowestCommonAncestor(self, root, p, q):
+        """
+        :type root: TreeNode
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: TreeNode
+        """
+        # 6 star, no idea
+        # 递归解决，向左右子树递归查找，直到找到p或q, 此时left和right的值有两种情况，一种是一个是lca一个是None, 另一种是分别是p和q
+        if not root or root in (p, q):
+            return root
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
+        if left and right:
+            return root
+        return left or right
