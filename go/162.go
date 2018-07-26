f0588bdaf82e03b9d45f12f8305f0575291935cf
along <along@myw-vicdeiMac.local>
2018-07-26 15:33:25 +0800

    add 162

---
+++ go/162.go
@@ -0,0 +1,31 @@
+
+//package main
+//
+//import "fmt"
+
+// 1 star
+// slow, beat 2%, 可以改为使用二分法
+func findPeakElement(nums []int) int {
+	length := len(nums)
+	if length == 1 {return 0}
+	for index, _ := range nums {
+		if index == 0 {
+			if nums[index] > nums[index+1] {
+				return index}
+		} else if index == length-1 {
+			if nums[index] > nums[index-1] {
+				return index}
+		} else {
+			if nums[index] > nums[index-1] && nums[index] > nums[index+1] {
+				return index
+			}
+		}
+	}
+	return 0
+}
+
+
+//func main() {
+//	nums := []int {1}
+//	fmt.Println(findPeakElement(nums))
+//}
