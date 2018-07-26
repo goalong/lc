8afe2eb044d8f14230affddeaf1e7064d5e35c23
along <along@myw-vicdeiMac.local>
2018-07-26 16:33:47 +0800

    add 55

---
+++ go/55.go
@@ -0,0 +1,31 @@
+
+//package main
+//
+//import "fmt"
+
+// 6 star, 用一个与nums等长的切片canArrive记录每个索引能否被跳到，从左到右遍历nums,根据每个索引上的步数更新canArrive上对应索引的值
+//最后判断canArrive上最后一位的值是否为1
+func canJump(nums []int) bool {
+	var steps int
+	canArrive := make([]int, len(nums))
+	canArrive[0] = 1
+	length := len(nums)
+	index := 0
+	for index < length && canArrive[index] == 1 {
+		steps = nums[index]
+		for idx:=1; idx<=steps;idx++{
+			if index + idx >= length - 1 {return true}
+			canArrive[index+idx] = 1
+		}
+		index += 1
+	}
+	return canArrive[length-1] == 1
+
+}
+
+
+//func main() {
+//	nums := []int {2,3,1,1,4}
+//	ret := canJump(nums)
+//	fmt.Println(ret)
+//}
