e4e3af6f22fdd28915c58f94e29937d5f0228beb
along <along@myw-vicdeiMac.local>
2018-07-26 16:34:18 +0800

    update

--- go/153.go
+++ go/153.go
@@ -1,5 +1,5 @@
 
-//package main
+package main
 
 //import "fmt"
 
--- go/219.go
+++ go/219.go
@@ -1,4 +1,5 @@
 
+package main
 // 4 star, 使用一个map记录值到索引的映射，当有重复的时候，检查它们索引的差，小于等于k则表明存在，大于k则更新该值的索引为新的索引
 func containsNearbyDuplicate(nums []int, k int) bool {
 	numMap := make(map[int]int)
