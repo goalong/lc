1c889ab4a6d79629d7d0b59186febdd8b7a3ea3e
along <along@myw-vicdeiMac.local>
2018-08-13 16:33:01 +0800

    add 198

---
+++ go/198.go
@@ -0,0 +1,28 @@
+
+// package main
+
+// 5 star, 动态规划
+func rob(nums []int) int {
+	length := len(nums)
+	if length == 0 {
+		return 0
+	} else if length == 1 {
+		return nums[0]
+	}
+	dp := make([]int, length)
+	dp[0] = nums[0]
+	if nums[1] > nums[0] {
+		dp[1] = nums[1]
+	} else {
+		dp[1] = nums[0]
+	}
+	for i:=2; i< length; i++ {
+		if dp[i-2] + nums[i] > dp[i-1] {
+			dp[i] = dp[i-2] + nums[i]
+		} else {
+			dp[i] = dp[i-1]
+		}
+	}
+	return dp[length-1]
+
+}
