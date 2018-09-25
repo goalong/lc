690a27f8dd2bbb398854fa9a9fe2a4ea1828a0a2
along <along@myw-vicdeiMac.local>
2018-09-25 15:22:47 +0800

    add comment

--- v2/64.py
+++ v2/64.py
@@ -8,6 +8,7 @@ class Solution:
         :rtype: int
         """
         # 6 star 动态规划, 需要注意先计算最上和最右的行和列
+        # 状态转移方程： dp[i][j] = min(dp[i-1][j], dp[i][j-1]) + grid[i][j]
         m, n = len(grid), len(grid[0])
         dp = [[0 for _ in range(n)] for _ in range(m)]
         dp[0][0] = grid[0][0]
